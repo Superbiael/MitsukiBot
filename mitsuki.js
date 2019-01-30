622030b6d150361328645e0e066aa215159525e7
Superbiael <39025166+Superbiael@users.noreply.github.com>
2019-01-30 04:32:01 -0800

    Update mitsuki.js

--- mitsuki.js
+++ mitsuki.js
@@ -5,7 +5,7 @@ bot.commands = new Discord.Collection();
 
    bot.on("ready", async () => {
    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-   bot.user.setActivity("Associate", {type:0});
+   bot.user.setActivity("Associate", {type:"LISTENING"});
   });
 
    bot.on('message', message => {

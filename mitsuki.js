9d2d75951a2bd3c6f9ea157bcf27e45c260c26f3
Superbiael <39025166+Superbiael@users.noreply.github.com>
2019-03-31 04:21:24 -0700

    Update mitsuki.js

--- mitsuki.js
+++ mitsuki.js
@@ -48,7 +48,7 @@ bot.commands = new Discord.Collection();
       }
 
    if (msg.startsWith (prefix + "scout")) {
-     number = 52;
+     number = 54;
      imageNumber = Math.floor (Math.random() * (number)) + 1;
      return message.channel.send ({files: ["./images/scout/" + imageNumber + ".png"]})
     }
